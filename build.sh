e3ecf1614858dce74f12e1d77602ad343c41e8e7
Luke Shingles <luke.shingles@gmail.com>
2024-02-29 16:26:50 +0000

    Update

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 xelatex cv-lukeshingles
 pdflatex publications-lukeshingles
 
-"/System/Library/Automator/Combine PDF Pages.action/Contents/MacOS/join" -o shingles-cv-publist.pdf cv-lukeshingles.pdf publications-lukeshingles.pdf
+#"/System/Library/Automator/Combine PDF Pages.action/Contents/MacOS/join" -o shingles-cv-publist.pdf cv-lukeshingles.pdf publications-lukeshingles.pdf
 
-rm -f *.log *.aux *.out *.fls *.fdb_latexmk
+gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=shingles-cv-publist.pdf -dBATCH cv-lukeshingles.pdf publications-lukeshingles.pdf
+
+rm -f *.log *.aux *.out
